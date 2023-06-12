5467633919127791b3ee7ca750975f14ee7ef88b
Tesierroe <tesierroe@gmail.com>
2023-06-12 16:22:54 +0300

    Controlna updates

--- main.py
+++ main.py
@@ -90,9 +90,7 @@ class PdfAnalyzer(FileAnalyzer):
             for page_num in range(num_pages):
                 page = pdf_reader.pages[page_num]
                 page_text = page.extract_text()
-                page_links = re.findall(
-                    r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', page_text
-                )
+                page_links = re.findall(r'(https?:\/\/[\da-z\.-]+\.[a-z\.]{2,6}[\/\w\.-]*)\/?', page_text)
                 links.extend(page_links)
         return links
 
